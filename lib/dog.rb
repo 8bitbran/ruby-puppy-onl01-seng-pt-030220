41f11f9fa8a6c53e659c22cc138188ad9d631add
Brandon Marrero <brandonmar97@gmail.com>
2020-03-23 13:40:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,20 @@
 # Add your code here
 class Dog 
   
+  #holds all dogs
   @@dogs = []
   
   def initialize(name)
     @name = name 
+    #adds each new dog to array of dogs
     @@dogs << self
   end 
+  
+  #returns all dogs
+  def self.all 
+    @@dogs
+  end
+
+  #clears array of dogs
 
 end 
